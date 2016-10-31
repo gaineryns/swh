4a542fbe80a61372074c44311092c2b76dd591d2
lolovytch <laurent.olivier.clain@gmail.com>
2016-10-31 07:03:42 +0000

    new start

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([
+    {
+        first_name: 'steve',
+        last_name: 'yongwo',
+        username: 'gaineryns',
+        address: 'cameroun',
+        password: 'steve'
+    },
+    {
+        first_name: 'laurent',
+        last_name: 'clain',
+        username: 'l.clain',
+        address: 'reunion',
+        password: 'laurent'
+    },
+    {
+        first_name: 'samy',
+        last_name: 'guelaia',
+        username: 's.guelaia',
+        address: 'algerie',
+        password: 'samy'
+    },
+    {
+        first_name: 'walid',
+        last_name: 'bouzebra',
+        username: 'w.bouzebra',
+        address: 'algerie',
+        password: 'walid'
+    },
+    {
+        first_name: 'alexis',
+        last_name: 'de carvalho',
+        username: 'a.decarvalho',
+        address: 'portugal',
+        password: 'alexis'
+    },
+    {
+        first_name: 'celeste',
+        last_name: 'vividila',
+        username: 'c.vividila',
+        address: 'congo',
+        password: 'celeste'
+    },
+    ])
